ec1bb415504f502ea89f44d7569739dba47c0b54
vinhbuihd <vinhhd94@gmail.com>
2022-02-22 21:52:31 +0700

    upđate

--- project/js/main.js
+++ project/js/main.js
@@ -91,14 +91,6 @@ loginSinginBtn.addEventListener('click', function() {
 
 const headerEle = document.querySelector('header')
 
-// window.addEventListener('scroll', function() {
-//     if (document.documentElement.scrollTop > 50) {
-//     console.log(123);
-//     } else {
-//         console.log(456)
-//     }
-// })
-
 var lastScrollTop = 0;
 
 // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
