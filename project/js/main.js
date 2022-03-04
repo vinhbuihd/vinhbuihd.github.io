c65f89fdac21998c2901a26d4b80ea968b66a5be
vinhbuihd <vinhhd94@gmail.com>
2022-03-04 22:06:03 +0700

    update

--- project/js/main.js
+++ project/js/main.js
@@ -1,9 +1,9 @@
 
-// import { products} from './highlight-product'
+import { products } from "./highlight-product.js";
+
 
 // click header nav-item drop subnav
 const dropItem = document.querySelector('.drop-item')
-
 
 window.addEventListener('click', function(e){   
     if (dropItem.contains(e.target)){
@@ -114,6 +114,246 @@ window.addEventListener("scroll", function(){
     headerEle.classList.remove('bg-transparent')
    }
 }, false);
+
+
+// // click bật ra popup
+
+const buyBtns = document.querySelectorAll('.card-cart')
+const popup = document.querySelector('.product-popup')
+let cartList = JSON.parse(localStorage.getItem('cartList')) || []
+
+
+
+function deleteItem() {
+    console.log(212)
+}
+
+function editItem() {
+    console.log(456)
+}
+
+// render popup
+function popupRender(product) {
+    if (product) {
+        popup.innerHTML = `
+            <div class="popup-box">
+            <button type="button" class="btn-close" aria-label="Close"></button>
+            <div class="popup-item d-flex align-items-center">
+                <div class="popup-image">
+                    <img src="${product.image}" alt="${product.name}">
+                </div>
+                <div class="d-flex flex-column">
+                    <div class="popup-name">${product.name}</div>
+                    <div class="popup-price">${product.price.S.toLocaleString()}</div>
+                </div>
+            </div>
+            <div class="popup-picksize">
+                <div class="popup-title">Chọn size *</div>
+                <div class="form-group">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" value="S" name="flexRadioDefault" id="size-s" checked>
+                        <label class="form-check-label" for="size-s">
+                        Size S
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" value="M" name="flexRadioDefault" id="size-m">
+                        <label class="form-check-label" for="size-m">
+                        Size M + 5.000đ
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" value="L" name="flexRadioDefault" id="size-l">
+                        <label class="form-check-label" for="size-l">
+                        Size L + 10.000đ
+                        </label>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="popup-note">
+                <div class="popup-title">Ghi chú <span>(nếu có)</span>
+                </div>
+                <input class="popup-input" type="text" placeholder="Ghi chú cho quán">
+            </div>
+        
+            <div class="popup-bottom mt-16 d-flex align-items-center justify-content-center flex-column">
+                <div class="cart-item-button d-flex align-items-center">
+                    <div class="minus"><i class="fa-solid fa-circle-minus"></i></div>
+                    <div class="cart-item-quantity">${product.quantity}</div>
+                    <div class="plus"><i class="fa-solid fa-circle-plus"></i></div>
+                </div>
+                <button class="btn">Thêm vào giỏ hàng : <span>${(product.price.S * product.quantity).toLocaleString()}</span> đ</button>
+            </div>
+        </div>
+        `
+    }
+
+}
+
+// render product in cartside
+function cartSideRender(list) {
+    const cardSideElement = document.querySelector('.cart-side .content')
+    const cardIconQuantity = document.querySelector('.cart .cart-quantity')
+    
+    if (list.length == 0) {
+        cardSideElement.innerHTML = `
+            <h3>Giỏ hàng trống</h3>
+            <a href='../page/menu.html' class='btn'>Tiếp tục mua hàng</a>
+        `
+
+        cardIconQuantity.innerHTML = 0
+    }else {
+        cardIconQuantity.style.display = 'block'
+        cardSideElement.innerHTML = ''
+        let totalQuantity = 0
+        let totalPrice = 0
+        
+        
+        list.forEach((product) => {
+            totalPrice += product.quantity * product.sizePrice
+            totalQuantity += product.quantity
+
+            cardSideElement.innerHTML += `
+                <div class="cart-item d-flex" dataset=${product.code}>
+                    <div class="cart-item-image">
+                        <img src=${product.image} alt=${product.alt}>
+                    </div>
+                    <div class="cart-item-info">
+                        <div class="name">${product.name}</div> 
+                        <div class="size d-flex align-items-center">Size ${product.size}<span>x ${product.quantity}</span>
+                            <div class="edit" onclick='editItem()'><i class="fa-solid fa-pen"></i></div>
+                        </div>
+                        <div class="note">${product.note}</div>
+                    </div>
+                    
+                    <div class="d-flex align-items-center" >
+                        <div class="cart-item-sub-price">
+                            <span>${(product.quantity * product.sizePrice).toLocaleString()}</span> đ
+                        </div>
+                    </div>
+
+                    <div class="delete-product d-flex align-items-center" onclick='deleteItem()'>
+                        <i class="fa-solid fa-trash-can"></i>
+                    </div>
+                </div>
+            `
+        })
+        
+        cardSideElement.innerHTML += `
+            <a href="./page/menu.html">Thêm món</a>
+            <div class="sub-total d-flex align-items-center justify-content-between">
+                Tổng cộng <span>${totalPrice.toLocaleString()} đ</span>
+            </div>
+            <div class="cart-side-btn text-center mt-3">
+                <a href="./page/cart.html"class="btn">Thanh toán</a>
+            </div>
+        `
+
+        cardIconQuantity.innerHTML = totalQuantity
+    }
+}
+
+cartSideRender(cartList)
+
+// Xử lý khi nhấn mua hàng
+buyBtns.forEach(buyBtn => {
+    buyBtn.addEventListener('click', function (e) {
+        let card = e.target.closest('.card')
+        
+        let cardId = card.getAttribute('dataset')
+        
+        let product = products.filter(product => product.id == cardId)[0]
+        
+        // render sp trong popup
+        popup.style.display = 'block'
+
+        popupRender(product)
+
+        const minus = document.querySelector('.popup-bottom .minus')
+        const plus = document.querySelector('.popup-bottom .plus')
+        const popupQuantity = popup.querySelector('.popup-bottom .cart-item-quantity')
+        const popupBottomPrice = popup.querySelector('.popup-bottom .btn span')
+        const radios = document.querySelectorAll('.form-check-input')
+
+        radios.forEach(radio => {
+            radio.onchange = function () {
+                popupBottomPrice.innerHTML = (product.price[radio.value] * product.quantity).toLocaleString()
+            }
+        })
+
+        minus.addEventListener('click', function () {
+            let radio = Array.from(radios).find(radio => radio.checked)
+
+            if (product.quantity <=1 ) return
+            product.quantity--
+            popupQuantity.innerHTML = product.quantity
+            popupBottomPrice.innerHTML = (product.price[radio.value] * product.quantity).toLocaleString()
+        })
+
+        plus.addEventListener('click', function () {
+            let radio = Array.from(radios).find(radio => radio.checked)
+
+            product.quantity++
+            popupQuantity.innerHTML = product.quantity
+            popupBottomPrice.innerHTML = (product.price[radio.value] * product.quantity).toLocaleString()
+        })
+
+
+        const addToCart = document.querySelector('.popup-bottom .btn')
+        const closeCartBtn = document.querySelector('.popup-box .btn-close')
+        const popupNote = document.querySelector('.popup-input')
+    
+        addToCart.addEventListener('click', function () {
+            let radio = Array.from(radios).find(radio => radio.checked)
+            let newItem = {
+                ...product,
+                size: radio.value,
+                sizePrice: product.price[radio.value],
+                note: popupNote.value,
+                code: new Date().valueOf()
+            }
+            
+
+            // so sánh newItem đã có trong cartList hay chưa
+
+            if (cartList.length == 0) {
+                cartList.push(newItem)
+                localStorage.setItem('cartList', JSON.stringify(cartList));
+            } else {
+                let isEqual = cartList.find(product => (newItem.id == product.id && newItem.note == product.note && newItem.size == product.size))
+
+                if (isEqual == undefined) {
+                    console.log('no isEqual')
+                    cartList.push(newItem)
+                    localStorage.setItem('cartList', JSON.stringify(cartList));
+                } else {
+                    console.log(isEqual)
+                    isEqual.quantity += newItem.quantity
+                    localStorage.setItem('cartList', JSON.stringify(cartList));
+                }
+            }
+            
+            cartSideRender(JSON.parse(localStorage.getItem('cartList')))
+
+            product.quantity = 1
+            popup.style.display = 'none'
+            // overlay.style.display = 'none'
+        })
+        
+        closeCartBtn.addEventListener('click', function () {
+            popup.style.display = 'none'
+            product.quantity = 1
+            // overlay.style.display = 'none'
+        })
+    })
+})
+
+
+
+
+
+
 
 
 
